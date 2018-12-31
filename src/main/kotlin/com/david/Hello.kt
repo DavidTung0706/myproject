2beb8583f6b5b057ac0341cd91c64512e8e04ef7
董家成 <ddavid0706tw@gmail.com>
2018-12-31 11:57:22 +0800

    Initial commit

---
+++ src/main/kotlin/com/david/Hello.kt
@@ -0,0 +1,13 @@
+package com.david
+
+fun main(args: Array<String>) {
+//    println("Hello Kotlin")
+//     Human().hello()
+    val h = Human()
+    h.hello()
+}
+class Human{
+    fun hello(){
+        println("Hello Kotlin")
+    }
+}
