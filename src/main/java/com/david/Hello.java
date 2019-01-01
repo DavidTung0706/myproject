9ecffa10e987e4604168db56034e51c92e11cf53
董家成 <ddavid0706tw@gmail.com>
2019-01-01 23:34:48 +0800

    Java and Kotlin variable，data type

--- src/main/java/com/david/Hello.java
+++ src/main/java/com/david/Hello.java
@@ -3,18 +3,21 @@ package com.david;
 public class Hello {
     public static void main(String[] args) {
 //        System.out.println("Hello Java");
-        Person p = new Person();
+        Person p = new Person("David",66.5f,1.7f);
         p.hello();
-        int age = 19;
+        p.weight = 66.5f;
+        p.height = 1.7f;
+        System.out.println(p.bmi());
+
+        /*int age = 19;
         Integer age2 = 19;
         char c = 'A';
         Character c2 = 'B';
         byte b = 120;
         float weight = 66.5f;
         float height = 1.7f;
-
         boolean adult = true;
-        boolean enroll = false;
+        boolean enroll = false;*/
 
         String name = "David";
     }
--- src/main/kotlin/com/david/Hello.kt
+++ src/main/kotlin/com/david/Hello.kt
@@ -1,18 +1,32 @@
 package com.david
 
+import javafx.scene.text.FontWeight
+
 fun main(args: Array<String>) {
 //    println("Hello Kotlin")
 //     Human().hello()
-    val h = Human()
-    h.hello()
-    var age = 19
+    val h = Human(weight = 66.5f,height = 1.7f)
+    println(h.bmi())
+
+    /*var age = 19
     age = 20
     var weight = 66.5f
     var name : String
-    name = "David"
+    name = "David"*/
 
 }
-class Human{
+class Human(var name:String = "",var weight:Float , var height:Float ){
+    init {
+        println("test$weight")
+    }
+
+//    constructor(name:String,weight: Float,height: Float):this(weight,height)
+
+    fun bmi():Float{
+        val bmi = weight /(height*height)
+        return  bmi
+    }
+
     fun hello(){
         println("Hello Kotlin")
     }
