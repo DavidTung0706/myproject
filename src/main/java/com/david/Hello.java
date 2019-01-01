efa7474eda93d58d0e216a24cc013664284965e3
董家成 <ddavid0706tw@gmail.com>
2019-01-01 22:03:44 +0800

    Java and Kotlin variable，data type

--- src/main/java/com/david/Hello.java
+++ src/main/java/com/david/Hello.java
@@ -3,7 +3,19 @@ package com.david;
 public class Hello {
     public static void main(String[] args) {
 //        System.out.println("Hello Java");
-         Person p = new Person();
-         p.hello();
+        Person p = new Person();
+        p.hello();
+        int age = 19;
+        Integer age2 = 19;
+        char c = 'A';
+        Character c2 = 'B';
+        byte b = 120;
+        float weight = 66.5f;
+        float height = 1.7f;
+
+        boolean adult = true;
+        boolean enroll = false;
+
+        String name = "David";
     }
 }
